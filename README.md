f8d331fd58ca945da760e6f5ae6b9ff6f747e468
Leonard Hecker <leonard@hecker.io>
2016-10-30 14:17:42 +0100

    Added JSON baseline benchmark

--- README.md
+++ README.md
@@ -8,3 +8,24 @@ This is a PoC.
 npm install
 node index.js
 ```
+
+## Results
+
+name                           | samples | ops/sec | ns/op   | rme
+-------------------------------|---------|---------|---------|-------
+decode proto2 message1 **old** |      20 |  168516 |    5934 | ±0.42%
+decode proto2 message1 **new** |      23 |  341186 |    2931 | ±0.25%
+decode proto2 message2 **old** |      19 |     138 | 7241664 | ±3.98%
+decode proto2 message2 **new** |      20 |     203 | 4915836 | ±0.82%
+decode proto3 message1 **old** |      22 |  119309 |    8382 | ±2.33%
+decode proto3 message1 **new** |      21 |  189197 |    5286 | ±2.94%
+encode proto2 message1 **old** |      21 |  207638 |    4816 | ±0.32%
+encode proto2 message1 **new** |      20 |  206339 |    4846 | ±2.07%
+encode proto2 message2 **old** |      22 |     372 | 2688337 | ±0.71%
+encode proto2 message2 **new** |      20 |     385 | 2599718 | ±2.53%
+encode proto3 message1 **old** |      19 |  121546 |    8227 | ±0.19%
+encode proto3 message1 **new** |      20 |  121626 |    8222 | ±2.47%
+decode json message1           |      22 |  187882 |    5322 | ±0.22%
+decode json message2           |      19 |     327 | 3061492 | ±5.54%
+encode json message1           |      19 |  225345 |    4438 | ±1.97%
+encode json message2           |      22 |     489 | 2047071 | ±0.18%
--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const proto3New = require('./proto/benchmark_messages_proto3_pb_new');
 
 let datasetMessage1 = new Uint8Array(fs.readFileSync('./datasets/google_message1.dat'));
 let datasetMessage2 = new Uint8Array(fs.readFileSync('./datasets/google_message2.dat'));
+let jsonGoogleMessage1Encoded = fs.readFileSync('./datasets/google_message1.json', {encoding: 'utf8'});
+let jsonGoogleMessage2Encoded = fs.readFileSync('./datasets/google_message2.json', {encoding: 'utf8'});
+
+const suite = new Benchmark.Suite;
+const testSettings = {
+	maxTime: 1,
+};
 
 let proto2OldGoogleMessage1Decoded;
 let proto2NewGoogleMessage1Decoded;
@@ -15,57 +22,26 @@ let proto2OldGoogleMessage2Decoded;
 let proto2NewGoogleMessage2Decoded;
 let proto3OldGoogleMessage1Decoded;
 let proto3NewGoogleMessage1Decoded;
+suite.add('decode.proto2.message1.old', () => { proto2OldGoogleMessage1Decoded = proto2Old.GoogleMessage1.deserializeBinary(datasetMessage1); }, testSettings);
+suite.add('decode.proto2.message1.new', () => { proto2NewGoogleMessage1Decoded = proto2New.GoogleMessage1.deserializeBinary(datasetMessage1); }, testSettings);
+suite.add('decode.proto2.message2.old', () => { proto2OldGoogleMessage2Decoded = proto2Old.GoogleMessage2.deserializeBinary(datasetMessage2); }, testSettings);
+suite.add('decode.proto2.message2.new', () => { proto2NewGoogleMessage2Decoded = proto2New.GoogleMessage2.deserializeBinary(datasetMessage2); }, testSettings);
+suite.add('decode.proto3.message1.old', () => { proto3OldGoogleMessage1Decoded = proto3Old.GoogleMessage1.deserializeBinary(datasetMessage1); }, testSettings);
+suite.add('decode.proto3.message1.new', () => { proto3NewGoogleMessage1Decoded = proto3New.GoogleMessage1.deserializeBinary(datasetMessage1); }, testSettings);
 
-const suite = new Benchmark.Suite;
+suite.add('encode.proto2.message1.old', () => { proto2OldGoogleMessage1Decoded.serializeBinary(); }, testSettings);
+suite.add('encode.proto2.message1.new', () => { proto2NewGoogleMessage1Decoded.serializeBinary(); }, testSettings);
+suite.add('encode.proto2.message2.old', () => { proto2OldGoogleMessage2Decoded.serializeBinary(); }, testSettings);
+suite.add('encode.proto2.message2.new', () => { proto2NewGoogleMessage2Decoded.serializeBinary(); }, testSettings);
+suite.add('encode.proto3.message1.old', () => { proto3OldGoogleMessage1Decoded.serializeBinary(); }, testSettings);
+suite.add('encode.proto3.message1.new', () => { proto3NewGoogleMessage1Decoded.serializeBinary(); }, testSettings);
 
-
-suite.add('decode.proto2.message1.old', () => {
-	proto2OldGoogleMessage1Decoded = proto2Old.GoogleMessage1.deserializeBinary(datasetMessage1);
-});
-
-suite.add('decode.proto2.message1.new', () => {
-	proto2NewGoogleMessage1Decoded = proto2New.GoogleMessage1.deserializeBinary(datasetMessage1);
-});
-
-suite.add('decode.proto2.message2.old', () => {
-	proto2OldGoogleMessage2Decoded = proto2Old.GoogleMessage2.deserializeBinary(datasetMessage2);
-});
-
-suite.add('decode.proto2.message2.new', () => {
-	proto2NewGoogleMessage2Decoded = proto2New.GoogleMessage2.deserializeBinary(datasetMessage2);
-});
-
-suite.add('decode.proto3.message1.old', () => {
-	proto3OldGoogleMessage1Decoded = proto3Old.GoogleMessage1.deserializeBinary(datasetMessage1);
-});
-
-suite.add('decode.proto3.message1.new', () => {
-	proto3NewGoogleMessage1Decoded = proto3New.GoogleMessage1.deserializeBinary(datasetMessage1);
-});
-
-suite.add('encode.proto2.message1.old', () => {
-	datasetMessage1 = proto2OldGoogleMessage1Decoded.serializeBinary();
-});
-
-suite.add('encode.proto2.message1.new', () => {
-	datasetMessage1 = proto2NewGoogleMessage1Decoded.serializeBinary();
-});
-
-suite.add('encode.proto2.message2.old', () => {
-	datasetMessage2 = proto2OldGoogleMessage2Decoded.serializeBinary();
-});
-
-suite.add('encode.proto2.message2.new', () => {
-	datasetMessage2 = proto2NewGoogleMessage2Decoded.serializeBinary();
-});
-
-suite.add('encode.proto3.message1.old', () => {
-	datasetMessage1 = proto3OldGoogleMessage1Decoded.serializeBinary();
-});
-
-suite.add('encode.proto3.message1.new', () => {
-	datasetMessage1 = proto3NewGoogleMessage1Decoded.serializeBinary();
-});
+let jsonGoogleMessage1Decoded;
+let jsonGoogleMessage2Decoded;
+suite.add('decode.json.message1', () => { jsonGoogleMessage1Decoded = JSON.parse(jsonGoogleMessage1Encoded); }, testSettings);
+suite.add('decode.json.message2', () => { jsonGoogleMessage2Decoded = JSON.parse(jsonGoogleMessage2Encoded); }, testSettings);
+suite.add('encode.json.message1', () => { jsonGoogleMessage1Encoded = JSON.stringify(jsonGoogleMessage1Decoded); }, testSettings);
+suite.add('encode.json.message2', () => { jsonGoogleMessage2Encoded = JSON.stringify(jsonGoogleMessage2Decoded); }, testSettings);
 
 suite.on('cycle', (event) => {
 	const bench = event.target;
@@ -86,7 +62,6 @@ suite.on('cycle', (event) => {
 });
 
 console.log('Running...');
-Benchmark.options.maxTime = 1; // benchmark.js does not allow this to be set using run()
 suite.run();
 
 function leftPad(num, length, padChar) {
